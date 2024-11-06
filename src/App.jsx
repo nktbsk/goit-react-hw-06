17dd852d363ea508655456cd143da10235a463e7
nktbsk <nikita.bashkir01@gmail.com>
2024-11-06 16:41:31 +0200

    HomeWork

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <></>;
-};
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
